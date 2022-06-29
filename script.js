099afa27b78458f3751c78f2e51a47c1bed8d482
LoiseMulwa <loisemulwa7@gmail.com>
2022-06-30 01:26:12 +0300

    ft:Initial code

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $("#card1").mouseover(function () {
+        $("#img1").show();
+      }).mouseout(function () {
+        $("#img1").hide();
+      });
+    })
+    $("#card3").mouseover(function () {
+        $("#img2").show();
+      }).mouseout(function () {
+        $("#img2").hide();
+      });
+  
+    $("#card2").mouseover(function () {
+        $("#img3").show();
+      }).mouseout(function () {
+        $("#img3").hide();
+      });
+  
